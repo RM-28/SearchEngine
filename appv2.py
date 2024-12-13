be842467a5a85af1cfa757d3d071e168464c731e
mattyu1 <38786876+mattyu1@users.noreply.github.com>
2024-12-13 12:41:06 -0500

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -36,4 +36,4 @@ def index():
     return jsonify(results.text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
